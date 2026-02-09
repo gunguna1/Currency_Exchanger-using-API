5402fd91738931086d157251bcdb81d1e90e1fba
gunguna1 <karan26504@gmail.com>
2026-02-09 17:13:43 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <div className="bg-amber-200 text-amber-50">test check</div>
+
+    </>
+  )
+}
+
+export default App
